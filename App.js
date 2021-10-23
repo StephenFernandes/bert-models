56a6dfe5bdd615803ddec02e5c9fb45eb322c400
Stallone Fernandes <50883121+StallonesHyper@users.noreply.github.com>
2021-10-23 15:52:44 +0530

    Added React Component

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+mport "./App.css";
+import HomePage from "./Components/HomePage";
+
 const newFunction = () => {
   console.log("Stallone Fernandes");
 };
@@ -5,3 +8,13 @@ const newFunction = () => {
 //Function Called 
 newFunction();
 console.log("Another Pull Request Made");
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
