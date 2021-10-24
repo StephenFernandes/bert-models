692ee7f6ec38102bf4132cf6ef164ca110044826
StalloneFernandes <44518531+StalloneFernandes@users.noreply.github.com>
2021-10-24 14:19:59 +0530

    Updated App.js

--- App.js
+++ App.js
@@ -7,3 +7,11 @@ newFunction();
 console.log("Another Pull Request Made");
 
 
+function doSomething(x,z){
+  console.log("Takes A Parameters");
+  return x+z;
+}
+
+doSomething(10,20);
+//Invoking The Function
+
